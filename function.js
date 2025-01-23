25eb04d6ca56d26a9156fbffdcff3ac85038d175
wreckster1507 <sarthakaitha1507@gmail.com>
2025-01-23 11:35:41 +0530

    .

--- function.js
+++ function.js
@@ -1,13 +1,58 @@
 // write a function
 
-function printNumbers(){
-    for (let i = 1;i<=3;i++){
-        document.write(i, "<br>");
-        for (let j = 1;j<=4;j++){
-            document.write(j, " ");
-        }
-        document.write("<br>");
-    }
+// function printNumbers(){
+//     for (let i = 1;i<=3;i++){
+//         document.write(i, "<br>");
+//         for (let j = 1;j<=4;j++){
+//             document.write(j, " ");
+//         }
+//         document.write("<br>");
+//     }
+// }
+
+// printNumbers();
+
+// function g(name) {
+//     return `Hello ${name}`;
+// }
+
+// console.log(g("World")); // Hello World
+
+
+function add(a, b) {
+    return a + b;
 }
 
-printNumbers();
+let n1 = parseInt(prompt("Enter first number: "));
+let n2 = parseInt(prompt("Enter second number: "));
+
+console.log(add(n1, n2));
+
+
+
+// prime number
+function isPrime(num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+let number = parseInt(prompt("Enter a number: "));
+if (isPrime(number)) {
+    console.log(`${number} is a prime number`);
+} else {
+    console.log(`${number} is not a prime number`);
+}
+
+
+
+
+
+
+
