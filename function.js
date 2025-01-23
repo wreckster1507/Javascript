5731f9ffdcffd274ccfc038ee12d07d93696e5dd
wreckster1507 <sarthakaitha1507@gmail.com>
2025-01-23 10:54:56 +0530

    .

---
+++ function.js
@@ -0,0 +1,13 @@
+// write a function
+
+function printNumbers(){
+    for (let i = 1;i<=3;i++){
+        document.write(i, "<br>");
+        for (let j = 1;j<=4;j++){
+            document.write(j, " ");
+        }
+        document.write("<br>");
+    }
+}
+
+printNumbers();
